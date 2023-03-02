da3700b14c27e51d76c90978903fa751d658d465
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-02 23:13:27 +0200

    refactor: Refactor the project to multiple reusable functions

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use dotenv::dotenv;
 use reqwest;
 use serde::{Deserialize, Serialize};
 use std::env;
-use std::io::{stdin, stdout, Write};
+use std::io::{self, Write};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct OAIRequest {
     #[serde(rename = "prompt")]
     prompt: String,
@@ -13,8 +13,8 @@ struct OAIRequest {
     max_tokens: u16,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct OAIChoices {
+#[derive(Debug, Deserialize, Serialize)]
+struct OAICompletion {
     #[serde(rename = "text")]
     text: String,
     #[serde(rename = "index")]
@@ -25,7 +25,7 @@ struct OAIChoices {
     finish_reason: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 struct OAIResponse {
     #[serde(rename = "id")]
     id: Option<String>,
@@ -36,50 +36,80 @@ struct OAIResponse {
     #[serde(rename = "model")]
     model: Option<String>,
     #[serde(rename = "choices")]
-    choices: Vec<OAIChoices>,
+    choices: Vec<OAICompletion>,
+}
+
+#[derive(Debug)]
+enum OAIError {
+    Api(reqwest::Error),
+    Json(serde_json::Error),
+    Unknown(String),
+}
+
+impl From<reqwest::Error> for OAIError {
+    fn from(error: reqwest::Error) -> Self {
+        OAIError::Api(error)
+    }
+}
+
+impl From<serde_json::Error> for OAIError {
+    fn from(error: serde_json::Error) -> Self {
+        OAIError::Json(error)
+    }
+}
+
+fn get_input() -> Result<String, io::Error> {
+    let mut input = String::new();
+    print!("{}>{}", "Sql:".green(), " ".blue());
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut input)?;
+    Ok(input)
+}
+
+async fn generate_sql_code(prompt: &str, api_key: &str) -> Result<String, OAIError> {
+    let uri = "https://api.openai.com/v1/engines/text-davinci-001/completions";
+    let request = OAIRequest {
+        prompt: prompt.to_owned(),
+        max_tokens: 1000,
+    };
+    let auth_header_val = format!("Bearer {}", api_key);
+
+    let response = reqwest::Client::new()
+        .post(uri)
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .header(reqwest::header::AUTHORIZATION, &auth_header_val)
+        .json(&request)
+        .send()
+        .await?
+        .json::<OAIResponse>()
+        .await?;
+
+    Ok(response.choices[0].text.clone())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn main() -> Result<(), OAIError> {
     dotenv().ok();
-    let uri = "https://api.openai.com/v1/engines/text-davinci-001/completions";
-
-    let oai_token: String =
-        env::var("OPENAI_API_KEY").expect("Please set OPENAI_API_KEY environment variable");
-
-    let auth_header_val = format!("Bearer {}", oai_token);
-    let mut input = String::new();
+    let api_key = env::var("OPENAI_API_KEY").map_err(|_| {
+        OAIError::Unknown("Please set OPENAI_API_KEY environment variable".to_owned())
+    })?;
     loop {
-        print!("{}>{}", "Sql:".green(), " ".blue());
-        stdout().flush().unwrap();
-        stdin().read_line(&mut input)?;
-
-        let request = OAIRequest {
-            prompt: format!("Generate a Sql code for the given statement. {}", input),
-            max_tokens: 1000,
-        };
-
-        let response = reqwest::Client::new()
-            .post(uri)
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .header(reqwest::header::AUTHORIZATION, &auth_header_val)
-            .json(&request)
-            .send()
-            .await?;
-
-        if response.status().is_success() {
-            let oai_response: OAIResponse = response.json().await?;
-            println!("{}", oai_response.choices[0].text);
-        } else {
-            let status = response.status().as_u16();
-            let err_msg = response
-                .text()
-                .await
-                .unwrap_or_else(|_| "Unknown error".to_string());
-            println!("{} {}", "Error:".red().bold(), err_msg.red());
-            println!("{} {}", "Status:".red().bold(), status);
+        match get_input() {
+            Ok(input) if input.trim().is_empty() => continue,
+            Ok(input) => match generate_sql_code(
+                &format!("Generate a SQL code for the given statement. {}", input),
+                &api_key,
+            )
+            .await
+            {
+                Ok(sql_code) => println!("{}", sql_code),
+                Err(OAIError::Api(error)) => println!("{} {}", "API Error:".red().bold(), error),
+                Err(OAIError::Json(error)) => println!("{} {}", "JSON Error:".red().bold(), error),
+                Err(OAIError::Unknown(error)) => {
+                    println!("{} {}", "Unknown Error:".red().bold(), error)
+                }
+            },
+            Err(error) => return Err(OAIError::Unknown(format!("Input Error: {}", error))),
         }
-
-        input.clear();
     }
 }
