a69b0d65c8e0ecc3e146dcb62369e1229411ac3b
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-02 21:46:45 +0200

    feat: Define Request and Response Structs
    This commit adds the definition of the `OAIRequest`, `OAIChoices`, and `OAIResponse` structs that we will use to interact with the OpenAI API. We use the `serde_derive` crate to derive the serialization and deserialization traits for these structs. These structs will allow us to send requests to the OpenAI API and parse the responses that we receive. This commit does not change any existing functionality.

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,27 @@ use spinners::{Spinner, Spinners};
 use std::env;
 use std::io::{stdin, stdout, Write};
 
+#[derive(Serialize, Debug)]
+struct OAIRequest {
+    prompt: String,
+    max_tokens: u16,
+}
+
+#[derive(Deserialize, Debug)]
+struct OAIChoices {
+    text: String,
+    index: u8,
+    logprobs: Option<u8>,
+    finish_reason: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct OAIResponse {
+    id: Option<String>,
+    object: Option<String>,
+    created: Option<u64>,
+    model: Option<String>,
+    choices: Vec<OAIChoices>,
+}
+
 fn main() {}
