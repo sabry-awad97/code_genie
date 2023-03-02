5f2e6f347459461273ae50483dc37c175bddf787
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-02 21:43:18 +0200

    feat: Import necessary dependencies
    This commit adds the necessary dependencies that we will use to build our program. We imported `dotenv`, `hyper`, `hyper_tls`, `serde_derive`, `spinners`, `std::env`, and their corresponding modules. These dependencies will enable us to make HTTP requests, load environment variables, serialize and deserialize data structures, and display spinner animations to indicate when the program is busy. This commit does not change any existing functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+use dotenv::dotenv;
+use hyper::body::Buf;
+use hyper::{header, Body, Client, Request};
+use hyper_tls::HttpsConnector;
+use serde_derive::{Deserialize, Serialize};
+use spinners::{Spinner, Spinners};
+use std::env;
+use std::io::{stdin, stdout, Write};
+
+fn main() {}
