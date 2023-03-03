0bbde2aae8fc92d26729b0dbfd73c56d307b05e5
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-03 07:18:40 +0200

    refactor: Add refactor the cache field to its own struct

--- src/openai.rs
+++ src/openai.rs
@@ -1,15 +1,34 @@
-use std::{env, num::NonZeroUsize};
-
 use dotenv::dotenv;
 use lru::LruCache;
+use std::{env, num::NonZeroUsize};
 
 use crate::model::{OAIRequest, OAIResponse};
 
 const CACHE_SIZE: usize = 100;
 
+pub struct CodeCache {
+    cache: LruCache<String, String>,
+}
+
+impl CodeCache {
+    pub fn new() -> Self {
+        Self {
+            cache: LruCache::new(NonZeroUsize::new(CACHE_SIZE).unwrap()),
+        }
+    }
+
+    pub fn put(&mut self, prompt: String, code: String) {
+        self.cache.put(prompt, code);
+    }
+
+    pub fn get(&mut self, prompt: &str) -> Option<String> {
+        self.cache.get(prompt).cloned()
+    }
+}
+
 pub struct OpenAI {
     pub api_key: String,
-    pub cache: LruCache<String, String>,
+    pub cache: CodeCache,
 }
 
 impl OpenAI {
@@ -18,14 +37,14 @@ impl OpenAI {
         let api_key = env::var("OPENAI_API_KEY")
             .map_err(|_| String::from("Please set OPENAI_API_KEY environment variable"))?;
 
-        let cache = LruCache::new(NonZeroUsize::new(CACHE_SIZE).unwrap());
+        let cache = CodeCache::new();
 
         Ok(Self { api_key, cache })
     }
 
     pub async fn generate_code(&mut self, prompt: &str) -> Result<String, String> {
         if let Some(cached_result) = self.cache.get(prompt) {
-            return Ok(cached_result.clone());
+            return Ok(cached_result);
         }
 
         let uri = "https://api.openai.com/v1/engines/text-davinci-001/completions";
